f3bdb3b1b3ddfa52ea9607df203d83987e0b4058
Rogerio Colaco <colaco.rogerio@gmail.com>
2021-05-28 15:35:37 -0300

    - Criação do repositório

---
+++ src/main/java/com/project/bootcamp/exceptions/ExceptionsResponse.java
@@ -0,0 +1,15 @@
+package com.project.bootcamp.exceptions;
+
+public class ExceptionsResponse {
+
+    private String message;
+
+    public ExceptionsResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
